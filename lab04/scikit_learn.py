f3475bd12e86d14498615822b64e9b1116b7cd55
Long Chung Chan <longchungchan@v1040-wn-rt-a-49-87.campus-dynamic.uwaterloo.ca>
2019-03-30 01:51:50 -0400

    add cmd interface

--- lab04/scikit_learn.py
+++ lab04/scikit_learn.py
@@ -1,7 +1,8 @@
 import os.path
+import sys
 
-from sklearn import tree
 from tqdm import tqdm
+from sklearn import tree
 
 def prune(input_list):
     l = []
@@ -32,10 +33,10 @@ def turn_string_to_sample(inputString):
     samples = []
     leagueId_case = get_leagueid_switch()
 
-    for x in elems:
+    for x in elems[1:]:
         if x == "NULL" or x == "null" or x == "":
             samples.append(-1)
-        elif not x.isdigit:
+        elif not x.isdigit():
             samples.append(leagueId_case.get(x, -1))
         else:
             samples.append(int(x))
@@ -47,8 +48,9 @@ def generate_sample_label(csvLocation):
     labels = []
 
     samples_csv = open(csvLocation, 'r')
+    samples_csv.readline()
 
-    for entry in tqdm(samples_csv):
+    for entry in samples_csv:
         playerId, features, label = turn_string_to_sample(entry)
 
         samples.append(features)
@@ -57,9 +59,8 @@ def generate_sample_label(csvLocation):
     samples_csv.close
     return samples, labels
 
-if __name__ == "__main__":
-
-    dataset = generate_sample_label("samples.csv")
+def train_and_test_on_data(csvlocation):
+    dataset = generate_sample_label(csvlocation)
     split = int(len(dataset[0]) * 0.8)
 
     # training
@@ -75,10 +76,23 @@ if __name__ == "__main__":
             correct += 1
         cnt += 1
     
-    print("Total sample tested: ", cnt)
-    print("Total correct answer:", correct)
-    print("Correct Percentage", (correct/cnt)*100)
+    accuracy = (correct/cnt)*100
+    #print("Total: ", cnt, "Correct: ", correct, "Accuracy: ", accuracy)
 
+    return accuracy
 
+def multiple_data_set(iteration, csv_locations):
+    accuracy = [0]*len(csv_locations)
 
+    for _ in tqdm(range(iteration)):
+        for u in range(len(accuracy)):
+            accuracy[u] += train_and_test_on_data(csv_locations[u])
+    
+    for u in range(len(accuracy)):
+        print("Avg: ", accuracy[u]/iteration, "CSV: ", csv_locations[u])
+
+if __name__ == "__main__":
+    i = int(sys.argv[1])
+    csv_locations = sys.argv[2:]
+    multiple_data_set(i, csv_locations)
 
